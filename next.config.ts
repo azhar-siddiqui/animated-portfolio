d43caa2ee393bffcba2a0e747069eea10f28cae4
Zahid Shaikh <zahid.786shaikh@gmail.com>
2025-09-15 13:41:30 +0530

    fix: turbopack file trace root issue fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.resolve(__dirname),
   /* config options here */
+  
 };
 
 export default nextConfig;
